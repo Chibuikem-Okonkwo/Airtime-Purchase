33c207d1414334185b6c0af68491d489703ad039
Chibuikem-Okonkwo <100425157+Chibuikem-Okonkwo@users.noreply.github.com>
2022-08-18 16:39:16 +0100

    Add files via upload
    Uploade Scenes For Airtime Purchase for a Mobile App made with Unity and C#

---
+++ Assets/Scripts/SceneConstants.cs
@@ -0,0 +1,7 @@
+public class SceneConstants
+{
+
+    public static string Home = "Home";
+    public static string AirtimePurchase = "AirtimePurchase";
+    public static string Confirm = "Confirm";
+}
